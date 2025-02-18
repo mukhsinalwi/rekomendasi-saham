199bd12a9f1a019c4b9109f0d835bc9a210306b2
mukhsinalwi <mukhsinalwi123@gmail.com>
2025-02-18 17:27:22 +0700

    Add files via upload
    kode untuk rekomendasi saham yang worth it atau tidak untuk dibeli

---
+++ RekomendasiSaham.py
@@ -0,0 +1,11 @@
+
+
+input_user = str(input("masukan saham keinginanmu"))
+if (input_user == "ptro" or input_user == "cuan" or input_user == "ssia" or input_user == "irsx" or input_user == "mina"):
+    print("worth it to buy")
+else :
+    print("don't buy!!!")
+
+
+
+
